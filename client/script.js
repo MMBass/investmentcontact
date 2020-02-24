a10f44ded840094941a76c70fee2d8e4687d2479
MMBass <51884406+MMBass@users.noreply.github.com>
2020-02-25 00:41:35 +0200

    first after delete and create again

---
+++ client/script.js
@@ -0,0 +1,61 @@
+document.getElementById('calcBtn').addEventListener('click', () => {
+    value = document.getElementById('amountInput').value;
+    callApi(Number(value));
+});
+
+function callApi(userVal) {
+    let resList = {};
+    if (typeof userVal === 'number' && userVal !== 0) {
+
+        fetch('http://localhost:3000/thirtyFive')
+            .then((res) => {
+                return res = res.text();
+            })
+            .then((thirtyFive) => {
+                resList.thirtyFive = parseFloat(thirtyFive);
+
+                fetch('http://localhost:3000/hundred')
+                    .then((res) => {
+                        return res = res.text();
+                    })
+                    .then((hundred) => {
+                        resList.hundred = parseFloat(hundred);
+
+                        fetch('http://localhost:3000/allShare')
+                            .then((res) => {
+                                return res = res.text();
+                            })
+                            .then((allShare) => {
+                                resList.allShare = parseFloat(allShare);
+                              
+                                calcResults(userVal, resList);
+                            })
+                    })
+            })
+        // calcResults(userVal, {
+        //     thirtyFive: [20, 40, 50],
+        //     hundred: [10, 50, 80],
+        //     rest: [20, 30, 40]
+        // });
+    }
+
+}
+
+function calcResults(userVal, fromApi) {
+
+    let aResult = userVal + ((userVal / 100) * fromApi.thirtyFive);
+    let bResult = userVal + ((userVal / 100) * fromApi.hundred);
+    let cResult = userVal + ((userVal / 100) * fromApi.allShare);
+
+    results = [aResult, bResult, cResult]
+    displayRes(results);
+}
+
+function displayRes(results) {
+    document.querySelector('#resultsArea').style.display = 'block';
+    let divs = document.querySelectorAll('.resultDiv');
+    for (i = 0; i < divs.length; i++) {
+        divs[i].innerHTML = `<p>12 month: ${(results[i]).toFixed(2)}</p>`
+    }
+    // <br> <p>3 month: ${(results[i] * 3).toFixed(2)}</p><br> <p> 6 month: ${(results[i] * 6).toFixed(2)}</p>
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const fetch = require('node-fetch');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const PORT = process.env.PORT||3000;
+
+app.use(cors())
+app.use(express.static('../'));
+app.get('/thirtyFive',(req,res)=>{
+
+    fetch('https://www.bizportal.co.il/capitalmarket/indices/performance/33343333')
+    .then(res => res.text())
+    .then(body => {
+         const resDom = new JSDOM(body);
+         let thirtyFive = resDom.window.document.querySelectorAll('.statistics-container ul .num')[4].textContent;
+
+         res.send(thirtyFive);
+    });
+})
+
+app.get('/hundred',(req,res)=>{
+    fetch('https://www.bizportal.co.il/capitalmarket/indices/generalview/33333333')
+    .then(res =>res.text())
+    .then(body => {
+         const resDom = new JSDOM(body);
+         let hundred = resDom.window.document.querySelectorAll('.statistics-container ul .num')[4].textContent;
+
+         res.send(hundred);
+    })
+});
+
+app.get('/allShare',(req,res)=>{
+    fetch('https://www.bizportal.co.il/capitalmarket/indices/generalview/523')
+    .then(res =>res.text())
+    .then(body => {
+         const resDom = new JSDOM(body);
+         let allShare = resDom.window.document.querySelectorAll('.statistics-container ul .num')[4].textContent;
+
+         res.send(allShare);
+    })
+});
+
+app.listen(PORT, ()=>console.log(`listening on port ${PORT}...`));
