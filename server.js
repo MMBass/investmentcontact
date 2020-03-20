5aaa2972bfc47aec5c863b71891964c5ada46d7b
MMBass <51884406+MMBass@users.noreply.github.com>
2020-03-20 15:46:36 +0200

    Catch in first fetch

--- server.js
+++ server.js
@@ -11,22 +11,9 @@ const PORT = process.env.PORT||3000;
 app.use(cors())
 app.use(express.static('client'));
 
-function exampleFetch(){
-    fetch('https://crownheights.info/contact-us')
-    .then(res => res.text())
-    .then(body => {
-         const resDom = new JSDOM(body);
-         console.log(resDom);
-         let t = resDom.window.document.querySelectorAll('h1')[0].textContent;
-console.log(t)
-   
-    });
-    
- }
-
 app.get('/thirtyFive',(req,res)=>{
-    exampleFetch();
     fetch('https://www.bizportal.co.il/capitalmarket/indices/performance/33343333')
+           
     .then(res => res.text())
     .then(body => {
          const resDom = new JSDOM(body);
@@ -34,7 +21,9 @@ app.get('/thirtyFive',(req,res)=>{
          let thirtyFive = resDom.window.document.querySelectorAll('.statistics-container ul .num')[4].textContent;
 
          res.send(thirtyFive);
-    });
+    }).catch(function(){
+        console.log(error);
+        }
 })
 
 app.get('/hundred',(req,res)=>{
