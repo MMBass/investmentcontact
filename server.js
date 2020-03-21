0f4c976318d6f9e65b2612741348791d45c1b91d
MMBass <51884406+MMBass@users.noreply.github.com>
2020-03-21 23:02:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,38 +12,55 @@ app.use(cors())
 app.use(express.static('client'));
 
 app.get('/thirtyFive',(req,res)=>{
+    
+    //bizportal link:
+    // https://www.bizportal.co.il/capitalmarket/indices/generalview/33333333
     fetch('https://www.tase.co.il/he/market_data/index/142/major_data')
            
     .then(res => res.text())
     .then(body => {
          const resDom = new JSDOM(body);
          console.log(resDom);
-       
-         let thirtyFive = resDom.window.document.querySelectorAll('.statistics-container ul .num')[4].textContent;
+        
+         //find from bizportal:
+         //let thirtyFive = resDom.window.document.querySelectorAll('.statistics-container ul .num')[4].textContent;
 
          res.send(thirtyFive);
     }).catch(function(error){
-        
+        console.log(error)
+         if(error){
+             
+         }
         });
 })
 
 app.get('/hundred',(req,res)=>{
-    fetch('https://www.bizportal.co.il/capitalmarket/indices/generalview/33333333')
+    
+    //bizportal link:
+    // https://www.bizportal.co.il/capitalmarket/indices/generalview/33333333
+    fetch('')
     .then(res =>res.text())
     .then(body => {
          const resDom = new JSDOM(body);
-         let hundred = resDom.window.document.querySelectorAll('.statistics-container ul .num')[4].textContent;
+        
+        //find from bizportal:
+         //let hundred = resDom.window.document.querySelectorAll('.statistics-container ul .num')[4].textContent;
 
          res.send(hundred);
     })
 });
 
 app.get('/allShare',(req,res)=>{
-    fetch('https://www.bizportal.co.il/capitalmarket/indices/generalview/523')
+    
+      //bizportal link:
+    // https://www.bizportal.co.il/capitalmarket/indices/generalview/523
+    fetch('')
     .then(res =>res.text())
     .then(body => {
          const resDom = new JSDOM(body);
-         let allShare = resDom.window.document.querySelectorAll('.statistics-container ul .num')[4].textContent;
+         
+         //find from bizportal:
+         //let allShare = resDom.window.document.querySelectorAll('.statistics-container ul .num')[4].textContent;
 
          res.send(allShare);
          console.log(allShare)
